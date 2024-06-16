ad052091d330ca313bff634889a1a2b3c3e03ab8
Amitsharma2468 <amitkmrsharma292@gmail.com>
2024-06-16 17:20:12 +0600

    structure

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div><h1 className=' font-bold text-green-600 text-3xl'>Welcome to LIFECYCLE</h1>
+    <a href="/login"><button className='bg-green-600 text-white font-bold px-3 py-2 rounded-md m-4'>Login</button></a>
+    </div>
+  )
+}
+
+export default HomePage
